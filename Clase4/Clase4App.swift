869b322763b90d95c717ad1c26057bef32a97fc1
422106685 <422106685@pcpum.acatlan.unam.mx>
2023-03-29 13:37:20 -0600

    Initial Commit

---
+++ Clase4/Clase4App.swift
@@ -0,0 +1,17 @@
+//
+//  Clase4App.swift
+//  Clase4
+//
+//  Created by CEDAM07 on 29/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Clase4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
